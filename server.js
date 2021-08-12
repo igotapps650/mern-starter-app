303e13e0b0b78bc2939f23956805bfdd829e09f7
igotapps650 <igotapps650@gmail.com>
2021-08-11 17:18:13 -1200

    configured for heroku deployment

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require('cors');
-require('dotenv').config();
-const PORT = 5000;
+const path = require("path");
+require("dotenv").config({path: "./config.env"});
+const PORT = process.env.PORT || 5000;
 
 app.use(cors({
   origin: "*",
@@ -31,6 +32,17 @@ try {
   );
 } catch (error) {
   console.log("could not connect");
+}
+
+//set static assets if in production
+if (process.env.NODE_ENV === 'production'){
+  //set static folder
+  app.use(express.static('client/build'));
+  
+  app.get("*",(req,res)=>{
+      res.sendFile(path.resolve(__dirname, 'client','build','index.html'));  
+  })
+
 }
 
 // importing routes
